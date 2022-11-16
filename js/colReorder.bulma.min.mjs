339de37920d043a711ff56cc1c1691c15c5694b2
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:10:16 +0000

    b03ec4b81a6e3d607437a6c23649032b63740d17
    Sync to source repo @03ec4b81a6e3d607437a6c23649032b63740d17

--- js/colReorder.bulma.min.mjs
+++ js/colReorder.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma styling wrapper for ColReorder
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import"datatables.net-colreorder";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-bm";import ColReorder from"datatables.net-colreorder";export default DataTable;
