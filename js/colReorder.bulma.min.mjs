ab38354d24fde1851d070877aa778b534d414dce
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-02 15:11:30 +0100

    1be9c134e5733f639465d2a2b14203c1ddb2a317 New: Dark mode support for DataTables and Bootstrap 5 styling
    Sync to source repo @1be9c134e5733f639465d2a2b14203c1ddb2a317

--- js/colReorder.bulma.min.mjs
+++ js/colReorder.bulma.min.mjs
@@ -1,4 +1,4 @@
 /*! Bulma styling wrapper for ColReorder
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bm";import ColReorder from"datatables.net-colreorder";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-bm";import ColReorder from"datatables.net-colreorder";let $=jQuery;export default DataTable;
